fe8b151798f8203d2e1cae0c658e24fe76d265f3
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-06-05 15:16:06 -0400

    update home index controller to redirect to new session path if not signed in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if current_user
+      render :index #this will change, will want to redirect to user's page. then can delete the view
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
