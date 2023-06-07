7d3cf3d22e9c48a46d8c4d9d0ee5771136097458
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-06-07 16:30:39 -0400

    add post model, avatar placeholder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_185548) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_200051) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_185548) do
     t.index ["user_id"], name: "index_friend_requests_on_user_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "profiles", force: :cascade do |t|
     t.string "username"
     t.string "avatar_url"
@@ -82,5 +91,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_185548) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "friend_requests", "users"
   add_foreign_key "friend_requests", "users", column: "friend_id"
+  add_foreign_key "posts", "users"
   add_foreign_key "profiles", "users"
 end
