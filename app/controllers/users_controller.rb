842f2317eded9fa3f397163c5daefc1c11d057ad
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-06-08 15:07:06 -0400

    add initial styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = Post.where(user_id: post_ids)
+    @posts = Post.where(user_id: post_ids).includes([:user]).includes([:image_attachment])
   end
 
   private
