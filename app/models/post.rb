07b7a60c77819691b5b3bccf05e86201ed3a7515
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-06-09 07:40:03 -0400

    add size, type validation to post image. add resize method

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,25 @@ class Post < ApplicationRecord
 
   validates_presence_of :title
   validates_presence_of :body
+
+  validate :acceptable_image
+
+  def resized_image
+    image.variant(resize_to_limit: [300, 300]).processed
+  end
+
+  private
+
+  def acceptable_image
+    return unless image.attached?
+  
+    unless avatar.blob.byte_size <= 1.megabyte
+      errors.add(:image, "is too big")
+    end
+  
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(avatar.content_type)
+      errors.add(:image, "must be a JPEG or PNG")
+    end
+  end
 end
