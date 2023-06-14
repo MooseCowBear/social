ec4f1855a1e0abed79068cfacf785e1eec63a424
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-06-14 09:06:31 -0400

    add date/time display to notification

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,8 +7,8 @@ module UsersHelper
     end
   end
 
-  def display_date(date)
-    zone = ActiveSupport::TimeZone.new(current_user.get_time_zone)
+  def display_date(date, user = current_user)
+    zone = ActiveSupport::TimeZone.new(user.get_time_zone)
     date = date.in_time_zone(zone)
     if date.today?
       date.strftime("%l:%M %P") #"today"
