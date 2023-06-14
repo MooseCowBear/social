2e5c484cf481e76e55830d3d5b719f1bea062a98
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-06-14 08:46:06 -0400

    change layout

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.notifications.includes([item: :user]) #add back in unread
+    @notifications = current_user.notifications.includes(item: [:user, :post]) #add back in unread
     @notifications.update(read: true)
   end
 end
