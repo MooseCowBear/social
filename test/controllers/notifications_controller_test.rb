9603d9925caa08dba24d60c3939c14f9ab615323
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-06-20 17:11:34 -0400

    add test for notifications controller

--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -1,8 +1,14 @@
 require "test_helper"
 
 class NotificationsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    sign_in users(:alice)
+  end
+
   test "should get index" do
-    get notifications_index_url
+    get notifications_path
     assert_response :success
   end
 end
