773b9240f6fd99460cda39175c0b81f8023bb30d
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-06-20 11:38:05 -0400

    add tests for creating posts

--- test/models/notification_test.rb
+++ test/models/notification_test.rb
@@ -1,8 +1,20 @@
 require "test_helper"
 
 class NotificationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-  
+  setup do
+    @notification = Notification.first
+  end
+
+  test "belongs to user" do 
+    assert @notification.user
+  end
+
+  test "belongs to item" do
+    assert @notification.item
+  end
+
+  test "unread scope returns correct number of notifications" do
+    n = Notification.unread
+    assert_equal 1, n.size
+  end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  
+  include ActionDispatch::TestProcess::FixtureFile
   setup do 
     @post = Post.first
   end
@@ -28,5 +28,44 @@ class PostTest < ActiveSupport::TestCase
 
   test "has the correct number of descendants" do
     assert_equal 2, @post.descendants.size
+  end
+
+  test "does not create post if no title" do
+    new_post = Post.new
+    new_post.body = "stuff"
+    new_post.user = users(:alice)
+
+    assert_not new_post.save
+  end
+
+  test "does not create post if no content" do
+    new_post = Post.new
+    new_post.title = "stuff"
+    new_post.user = users(:alice)
+
+    assert_not new_post.save
+  end
+
+  test "creates post if title and body" do
+    new_post = Post.new
+    new_post.title = "title"
+    new_post.body = "stuff"
+    new_post.user = users(:alice)
+
+    assert new_post.save
+  end
+
+  test "creates post if title and image" do
+    new_post = Post.new
+    new_post.title = "title"
+    new_post.user = users(:alice)
+    new_post.image = fixture_file_upload("branch.jpeg", "image/jpeg")
+
+    assert new_post.save
+  end
+
+  def after_teardown
+    super
+    FileUtils.rm_rf(ActiveStorage::Blob.service.root)
   end
 end
