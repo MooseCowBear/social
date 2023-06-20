f34e5a1faf26a79c007ab3e7cb7b03cbd80ae8f0
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-06-20 10:10:11 -0400

    add post, comment tests

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -4,4 +4,28 @@ class CommentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    @comment = Comment.last
+  end
+
+  test "belongs to user" do
+    assert @comment.user
+  end
+
+  test "has comments" do
+    assert @comment.comments
+  end
+
+  test "belongs to commentable" do
+    assert @comment.commentable
+  end
+
+  test "has notifications" do
+    assert @comment.notifications
+  end
+
+  test "has ancestor post" do
+    assert @comment.ancestor_post
+  end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  setup do 
+    @post = Post.first
+  end
+
+  test "belongs to user" do
+    assert @post.user
+  end
+
+  test "has comments" do
+    assert @post.comments
+  end
+
+  test "has notifications" do
+    assert @post.notifications
+  end
+
+  test "has likes" do
+    assert @post.likes
+  end
+
+  test "has descendants" do
+    assert @post.descendants
+  end
+
+  test "has the correct number of descendants" do
+    assert_equal 2, @post.descendants.size
+  end
 end
