49f38aee03c384efd2aad0e7da2a4de6b93dbc99
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-06-20 11:53:48 -0400

    add home controller tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,4 +1,19 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  test "redirects to log in page if user is not signed in" do
+    get "/"
+    assert_response :redirect
+    assert_redirected_to new_user_session_url
+  end
+
+  test "redirects to user's show page if signed in" do
+    sign_in users(:alice)
+
+    get "/"
+    assert_response :redirect
+    assert_redirected_to user_path(users(:alice).id)
+  end
 end
