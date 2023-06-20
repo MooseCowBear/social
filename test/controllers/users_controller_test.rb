a9f59d0dfa30f83b01fe6ef9e94e1781f20269b1
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-06-20 12:36:44 -0400

    add users controller tests. fix user, profile, post partials

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,31 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  include Devise::Test::ControllerHelpers
+  include Devise::Test::IntegrationHelpers
+
   setup do
-    @request.env["devise.mapping"] = Devise.mappings[:user]
     sign_in users(:alice)
+  end
+
+  test "should get index" do
+    get users_url
+    assert_response :success
+  end
+  
+  test "should get show for self" do
+    get user_path(users(:alice).id)
+    assert_response :success
+  end
+
+  test "should get show for friend" do
+    get user_path(users(:bob).id)
+    assert_response :success
+  end
+
+  test "should redirect if try to view page for non-friend" do
+    get user_path(users(:gary).id)
+    assert_response :redirect
+    assert_redirected_to root_path
   end
 
   
