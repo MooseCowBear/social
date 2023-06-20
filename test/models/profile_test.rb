59e42267087dfa6681058b921372c1f465fb3896
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-06-20 18:19:23 -0400

    add profile model tests

--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -4,5 +4,32 @@ class ProfileTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    @user = users(:frank)
+  end
   
+  test "does not create profile if username is more than 50 chars" do
+    profile = Profile.new
+    profile.user = @user
+    profile.username = "a" * 51
+
+    assert_not profile.save
+  end
+
+  test "does not create profile if username already exists" do
+    profile = Profile.new
+    profile.user = @user
+    profile.username = "alice's username"
+
+    assert_not profile.save
+  end
+
+  test "creates profile if username unique" do
+    profile = Profile.new
+    profile.user = @user
+    profile.username = "this username is unique"
+
+    assert profile.save
+  end
 end
