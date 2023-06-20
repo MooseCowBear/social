c0af09f6e8fdee518eda5f119b2e21221f85a418
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-06-20 10:13:29 -0400

    add like tests

--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  setup do 
+    @like = Like.last
+  end
+
+  test "belongs to user" do
+    assert @like.user
+  end
+
+  test "has notificaitons" do
+    assert @like.notifications
+  end
+
+  test "belongs to post" do
+    assert @like.post
+  end
 end
