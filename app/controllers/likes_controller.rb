4a353ce2eae70b6263b7256085a377ee40e94134
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-06-20 17:09:49 -0400

    add tests for likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,19 +2,18 @@ class LikesController < ApplicationController
   before_action :set_post
 
   def create
-    #duplicate likes (ie. same user, same post) are prevented 
-    #with model validations
-
     @like = @post.likes.new(user_id: current_user.id)
 
-    respond_to do |format|
       if @like.save
-        format.html { redirect_to post_path(@post), flash[:notice] = "Post has been liked." }
-        format.turbo_stream { flash.now[:notice] = "Post has been liked." }
+        respond_to do |format|
+          format.html { redirect_to post_path(@post), notice: "Post has been liked." }
+          format.turbo_stream { flash.now[:notice] = "Post has been liked." }
+        end
       else
-        format.html { redirect_to post_path(@post), flash[:alert] = "Like could not be saved." }
+        flash[:alert] = "Like could not be saved." 
+        redirect_to post_path(@post)
       end
-    end
+ 
   end
 
   def destroy
@@ -22,7 +21,7 @@ class LikesController < ApplicationController
     @like.destroy
 
     respond_to do |format|
-      format.html { redirect_to post_path(@post), flash[:notice] = "Post has been unliked." }
+      format.html { redirect_to post_path(@post), notice: "Post has been unliked." }
       format.turbo_stream { flash.now[:notice] = "Post has been unliked." }
     end
   end
--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -1,13 +1,29 @@
 require "test_helper"
 
 class LikesControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get likes_create_url
-    assert_response :success
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    sign_in users(:alice)
+    @post = Post.find(3)
   end
 
-  test "should get destroy" do
-    get likes_destroy_url
-    assert_response :success
+  test "should create like" do
+    assert_difference("Like.count", 1) do
+      post post_likes_path(post_id: @post.id), params: { post_id: @post.id }
+    end
+    assert_response :redirect
+    follow_redirect!
+    assert_equal "Post has been liked.", flash[:notice]
+  end
+
+  test "should delete like" do 
+    post = Post.find(1)
+    assert_difference("Like.count", -1) do
+      delete post_like_path(post_id: post.id, id: likes(:one).id)
+    end
+    assert_response :redirect
+    follow_redirect!
+    assert_equal "Post has been unliked.", flash[:notice]
   end
 end
