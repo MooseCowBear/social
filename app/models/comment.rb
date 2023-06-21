58327f3808d9fe35725d3c235410014995d1ea93
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-06-21 13:28:03 -0400

    update notification recipients for comments to include author of comment, add quotes around post titles in notification partials

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -15,6 +15,14 @@ class Comment < ApplicationRecord
   end
 
   def recipients
-    [Post.find_by(id: parent_post_id).user.id]
+    #notify the author of a post that there is a new comment
+    #and if that comment is a reply to another comment, notify
+    #that comment's author too. 
+    post_author_id = Post.find_by(id: parent_post_id).user.id
+    res = [post_author_id]
+    if commentable_type == "Comment"
+      res << Comment.find_by(id: commentable_id).user.id
+    end
+    res.uniq
   end
 end
