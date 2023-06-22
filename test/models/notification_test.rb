6cb51e4ef15864a87364bc8ef607a521060ee429
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-06-22 07:04:51 -0400

    add notification tests

--- test/models/notification_test.rb
+++ test/models/notification_test.rb
@@ -17,4 +17,51 @@ class NotificationTest < ActiveSupport::TestCase
     n = Notification.unread
     assert_equal 1, n.size
   end
+
+  test "number of notifications does not change if item is updated" do
+    assert_no_difference("Notification.count") do
+      post = Post.find(1)
+      post.update!(title: "first post, edited", body: "first post body, edited")
+    end
+  end
+
+  test "correct number of notifications are created when new post" do
+    assert_difference("Notification.count", 1) do
+      user = users(:alice)
+      user.posts.create!(title: "a new post!", body: "with some content")
+    end
+  end
+
+  test "correct number of notifications are created when post liked" do
+    assert_difference("Notification.count", 1) do
+      post = Post.find(1)
+      user = users(:bob)
+      like = Like.new
+      like.post = post
+      like.user = user
+      like.save!
+    end
+  end
+
+  test "correct number of notifications when comment on post" do
+    assert_difference("Notification.count", 1) do
+      comment = Comment.new
+      comment.user = users(:frank)
+      comment.commentable = Post.find(1)
+      comment.parent_post_id = 1
+      comment.body = "something"
+      comment.save!
+    end
+  end
+
+  test "correct number of notifications when reply to comment" do
+    assert_difference("Notification.count", 2) do
+      comment = Comment.new
+      comment.user = users(:frank)
+      comment.commentable = comments(:comment_two)
+      comment.parent_post_id = 1
+      comment.body = "something"
+      comment.save!
+    end
+  end
 end
